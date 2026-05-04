1f320c6d1d54ce8af5fb8bf577d61fbd8246e181
Test User <test@example.com>
2026-05-04 21:04:20 +0200

    fix: use type import for Document

--- server/models/Location.ts
+++ server/models/Location.ts
@@ -1,4 +1,4 @@
-import mongoose, { Schema, Document } from 'mongoose';
+import mongoose, { Schema, type Document } from 'mongoose';
 
 export interface ILocation extends Document {
   imageId: string;
